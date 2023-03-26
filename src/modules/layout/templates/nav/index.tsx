01d2b1f6cc3a4dcb0a46009a244280078e4ef225
gunkev <kevinenzapdi@gmail.com>
2023-03-27 00:24:34 +0100

    chore: navbar "change default navebar"

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -12,27 +12,6 @@ import { useEffect, useState } from "react"
 const Nav = () => {
   const { pathname } = useRouter()
   const [isHome, setIsHome] = useState(false)
-  const [isScrolled, setIsScrolled] = useState(false)
-
-  //useEffect that detects if window is scrolled > 5px on the Y axis
-  useEffect(() => {
-    if (isHome) {
-      const detectScrollY = () => {
-        if (window.scrollY > 5) {
-          setIsScrolled(true)
-        } else {
-          setIsScrolled(false)
-        }
-      }
-
-      window.addEventListener("scroll", detectScrollY)
-
-      return () => {
-        window.removeEventListener("scroll", detectScrollY)
-      }
-    }
-  }, [isHome])
-
   useEffect(() => {
     pathname === "/" ? setIsHome(true) : setIsHome(false)
   }, [pathname])
@@ -45,20 +24,10 @@ const Nav = () => {
         "!fixed": isHome,
       })}
     >
-      <header
-        className={clsx(
-          "relative h-16 px-8 mx-auto transition-colors bg-transparent border-b border-transparent duration-200 group-hover:bg-white group-hover:border-gray-200",
-          {
-            "!bg-white !border-gray-200": !isHome || isScrolled,
-          }
-        )}
-      >
+      <header className="relative h-16 px-8 mx-auto transition-colors bg-white border-b border-transparent duration-200 group-hover:bg-white group-hover:border-gray-200 border-b-[#f1f1f1]">
         <nav
           className={clsx(
-            "text-gray-900 flex items-center justify-between w-full h-full text-small-regular transition-colors duration-200",
-            {
-              "text-white group-hover:text-gray-900": isHome && !isScrolled,
-            }
+            "text-gray-900 flex items-center justify-between w-full h-full text-small-regular transition-colors duration-200"
           )}
         >
           <div className="flex-1 basis-0 h-full flex items-center">
@@ -72,7 +41,9 @@ const Nav = () => {
 
           <div className="flex items-center h-full">
             <Link href="/">
-              <a className="text-xl-semi uppercase">Acme</a>
+              <a className="text-xl-semi uppercase">
+                <img src="/logo.png" alt="" width={50} />
+              </a>
             </Link>
           </div>
 
